eb36e71c5cd8d9a3e285e98a702c36766f1a34a3
ID473 <dyomkin.i@yandex.ru>
2026-04-15 01:27:05 +0300

    Исправление серого фона при касании кнопок

--- miniapp/src/App.jsx
+++ miniapp/src/App.jsx
@@ -838,6 +838,10 @@ const styles = {
 
 const styleSheet = document.createElement("style");
 styleSheet.textContent = `
+    button {
+        -webkit-tap-highlight-color: transparent;
+        outline: none;
+    }
     @keyframes spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
